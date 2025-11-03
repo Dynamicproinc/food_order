792b7f2fe0ae51deb35ac1df3e5bf3891a536926
Lakmal Wijeartne <samdynmic@gmail.com>
2025-11-03 11:48:26 +0100

    dsds

--- resources/views/shop/cart.blade.php
+++ resources/views/shop/cart.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Cart')
 @section('content')
-    <div>
+    <div class="mb-5">
         {{-- @if(session('cart') && count(session('cart')> 0)) --}}
         
         @livewire('shop.cart')
