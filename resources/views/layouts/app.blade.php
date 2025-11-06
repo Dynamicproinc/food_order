4f4eeeb18062e4cb857f821350ff15fc14fa130f
Lakmal Wijeartne <samdynmic@gmail.com>
2025-11-06 11:25:21 +0100

    dsds

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
         <footer class="text-center py-3 d-flex justify-content-center align-items-center" style="height:100px">
   <div class="container ">
     <div>{{__('For support, contact')}} <a href="mailto:info@mbrothers-food.com" class="text-white">info@mbrothers-food.com</a></div>
-    <div>&copy;  {{ date('Y') }} {{ config('app.name') }}. All rights reserved.</div>
+    <div>&copy;  {{ date('Y') }} {{ config('app.name') }}. {{__('All rights reserved.')}}</div>
   </div>
 </footer>
 
