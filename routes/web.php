d2e18123a82db7954ff49bbf5c1d66a0e4746f13
Lakmal Wijeartne <samdynmic@gmail.com>
2025-11-16 21:20:28 +0100

    dsds

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,11 @@ Route::middleware(['auth', AdminMiddleware::class])->prefix('admin')->group(func
     
 });
 
+
+Route::get('/.env', function(Request $request) {
+    abort(404);
+});
+
 Route::get('/{any}', function($any) {
     if(str_starts_with($any, '.')) {
         abort(404);
