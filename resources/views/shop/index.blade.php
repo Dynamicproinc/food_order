2295c485ea6254f24c0eb21737e2ac9ddfa8afed
Lakmal Wijeartne <samdynmic@gmail.com>
2025-11-22 12:03:49 +0100

    Ds

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="col-4">
                     <div class="">
-                        <img src="{{ asset('images/design/banner_burger.png') }}" alt="burger" class="banner-image">
+                        <img src="{{ asset('images/design/banner_burger_main.png') }}" alt="burger" class="banner-image">
                     </div>
                 </div>
             </div>
