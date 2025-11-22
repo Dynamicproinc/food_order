a351c42be24f6a7fcd7f32e6eaf014b2b24ecbd2
Lakmal Wijeartne <samdynmic@gmail.com>
2025-11-22 11:46:42 +0100

    dsD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,7 @@
         {{--  --}}
         <footer class="text-center py-3 d-flex justify-content-center align-items-center" style="height:100px">
   <div class="container ">
+    <div><strong>{{ __('Open Hours: Monday to Friday, 10:00 – 14:00')}}</strong></div>
     <div>{{__('For support, contact')}} <a href="mailto:info@mbrothers-food.com" class="text-white">info@mbrothers-food.com</a></div>
     <div>&copy;  {{ date('Y') }} {{ config('app.name') }}. {{__('All rights reserved.')}}</div>
   </div>
