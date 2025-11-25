28960dfde48f8ee4e991c797d4e5269b317b9ae1
Lakmal Wijeartne <samdynmic@gmail.com>
2025-11-25 13:13:01 +0100

    dsds

--- resources/views/livewire/admin/kitchen.blade.php
+++ resources/views/livewire/admin/kitchen.blade.php
@@ -182,6 +182,11 @@
         // document.getElementById('newOrderNotification').style.display = 'flex';
         bell.play();
          @this.fetchOrders();
+
+        //  print 
+        // var divContents = document.getElementById(divId).innerHTML;
+   
+        // end print
         // alert('New order received: ' + event.data);
         // Optionally, you can refresh the page or update the order list dynamically here   
         // location.reload(); // Simple way to refresh the order list
