9170b70806e7cee716c6fa20fabc7cd28d2f231a
Lakmal Wijerathna <samdynmic@gmail.com>
2026-03-18 11:18:34 +0100

    dsd

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -40,7 +40,8 @@
     </div>
     
     <div class="d-flex justify-content-center mb-3 mt-3">
-        <div style="max-width:400px; overflow-x:auto;">
+            <div class="d-sm-block d-none">
+                        <div style="">
 
             @if (count($categories) > 0)
                 <div class="d-flex mb-3">
@@ -93,6 +94,62 @@
                 {{--  --}}
             @endif
         </div>
+            </div>
+            <div class="d-sm-none d-block">
+                        <div style="max-width:400px; overflow-x:auto;">
+
+            @if (count($categories) > 0)
+                <div class="d-flex mb-3">
+                    {{-- <a href="{{ route('shop.index')}}" class="menu-tab {{ request()->route('category') == null ? 'menu-tab-active' : '' }}">
+                       
+                        Sve
+                    </a> --}}
+                    <a href="{{ route('shop.index') }}" class="cat-link">
+                        <div class="cat-button">
+                            <div class="">
+                                <div class="d-flex justify-content-center">
+                                    <div class="icon  {{ request()->route('category') == null ? 'cat-active' : '' }}">
+                                        <img src="{{ asset('images/category/all.svg') }}" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="text">
+                                    sve
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    @foreach ($categories as $item)
+                        {{-- <a class="menu-tab {{ request()->route('category') == $item->category_name ? 'menu-tab-active' : '' }}" href="{{route('shop.category',$item->category_name)}}">
+                        {{$item->category_name}}
+                    </a> --}}
+                        <a href="{{ route('shop.category', $item->category_name) }}" class="cat-link">
+                            <div class="cat-button">
+                                <div class="">
+                                    <div class="d-flex justify-content-center">
+                                        <div
+                                            class="icon {{ request()->route('category') == $item->category_name ? 'cat-active' : '' }}">
+                                            <img src="{{ asset('images/category/' . $item->image) }}" alt=""
+                                                class="img-fluid">
+                                        </div>
+                                    </div>
+                                    <div class="text">
+                                        {{ str_replace('-', ' ', $item->category_name) }}
+
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+
+                </div>
+
+
+                {{--  --}}
+
+                {{--  --}}
+            @endif
+        </div>
+            </div>
     </div>
     @livewire('shop.index', ['products' => $products])
     </div>
