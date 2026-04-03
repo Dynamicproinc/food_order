e3145ff4668865fcaaa4b8f259f8d71c8b15fc89
Lakmal Wijerathna <samdynmic@gmail.com>
2026-04-03 13:21:15 +0200

    dsdS

--- resources/views/inc/modal/product.blade.php
+++ resources/views/inc/modal/product.blade.php
@@ -14,37 +14,43 @@
                         <div class="p-3">
                             <h5 class="title-md">{{ $selected_product->title }}</h5>
 
-                           @if ($selected_product->rating)
-                               <div>
-                             <div class="d-flex star-rating mb-2">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    @if ($i <= $selected_product->getRatingScore()['integer_part'])
-                                        <i class="bi bi-star-fill"></i>
-                                    @else
-                                        <i class="bi bi-star"></i>
-                                    @endif
-                                @endfor
-                                
-                                
-                               
-                                <span> ({{$selected_product->getRatingScore()['average_score']}}) | <a href="{{route('product.reviews', $selected_product->slug)}}"> {{$selected_product->getRatingScore()['total_ratings']}} {{__('Reviews')}}</a></span>
-                            </div>
-                           </div>
-                           @endif
-                           
-                            
-                            
-                              <div class="mb-3 d-flex">
-                                <h5 class="fw-bold mb-0">{{ number_format($selected_product->discounted_price, 2, ',', ' ') }} €</h5>
+                            @if ($selected_product->rating)
+                                <div>
+                                    <div class="d-flex star-rating mb-2">
+                                        @for ($i = 1; $i <= 5; $i++)
+                                            @if ($i <= $selected_product->getRatingScore()['integer_part'])
+                                                <i class="bi bi-star-fill"></i>
+                                            @else
+                                                <i class="bi bi-star"></i>
+                                            @endif
+                                        @endfor
+
+
+
+                                        <span> ({{ $selected_product->getRatingScore()['average_score'] }}) | <a
+                                                href="{{ route('product.reviews', $selected_product->slug) }}">
+                                                {{ $selected_product->getRatingScore()['total_ratings'] }}
+                                                {{ __('Reviews') }}</a></span>
+                                    </div>
+                                </div>
+                            @endif
+
+
+
+                            <div class="mb-3 d-flex">
+                                <h5 class="fw-bold mb-0">
+                                    {{ number_format($selected_product->discounted_price, 2, ',', ' ') }} €</h5>
                                 <div class="mx-3">
-                                   @if($selected_product->points > 0)
-                                 <span class="badge bg-success-subtle border border-success-subtle text-success-emphasis rounded-pill">{{__('Coupon applied')}}</span>
-                                @else
-                                 <span class="badge bg-dark-subtle border border-dark-subtle text-dark-emphasis rounded-pill">{{__('Coupon not applied')}}</span>
-                                @endif
-                               </div>
-                              </div>
-                            
+                                    @if ($selected_product->points > 0)
+                                        <span
+                                            class="badge bg-success-subtle border border-success-subtle text-success-emphasis rounded-pill">{{ __('Coupon applied') }}</span>
+                                    @else
+                                        <span
+                                            class="badge bg-dark-subtle border border-dark-subtle text-dark-emphasis rounded-pill">{{ __('Coupon not applied') }}</span>
+                                    @endif
+                                </div>
+                            </div>
+
                             <p>{{ $selected_product->description }}</p>
 
 
@@ -65,31 +71,42 @@
                                                 {{ App\Models\Options::where('id', $option_id)->first()->option_name }}
                                             </h6>
 
-                                            @foreach ($variants as $key => $item)
-                                                <div class="row align-items-center mb-2">
-                                                    <div class="col-6">
-                                                        <div class="form-check d-flex">
-                                                            <input class="form-check-input" type="radio"
-                                                                name="radioDefault_{{ $option_id }}"
-                                                                id="rd{{ $item->id }}" value="{{ $item->id }}"
-                                                                wire:model.live="variant.{{ $option_id }}"
-                                                                wire:click="calculateTotal"
-                                                                @if ($loop->first) checked @endif>
-                                                            <label class="form-check-label" for="rd{{ $item->id }}">
-                                                                <div>{{ $item->value }}</div>
-                                                                <p class="text-muted txt-xs" style="line-height: 1.2;">
-                                                                    {{ $item->description ?? $item->description }}</p>
-                                                            </label>
+                                            <div class="d-flex flex-row">
+                                                @foreach ($variants as $key => $item)
+                                                    <div class="mb-2">
+                                                        <div class="">
+                                                            <div class="fc-wrapper">
+                                                                <input class="fc-input" type="radio"
+                                                                    name="radioDefault_{{ $option_id }}"
+                                                                    id="rd{{ $item->id }}"
+                                                                    value="{{ $item->id }}"
+                                                                    wire:model.live="variant.{{ $option_id }}"
+                                                                    wire:click="calculateTotal"
+                                                                    @if ($loop->first) checked @endif>
+                                                                <label class="fc-label" for="rd{{ $item->id }}">
+                                                                    <div class="fc-title mb-0">{{ $item->value }}</div>
+                                                                      <small class="small txt-xs">{{ number_format($item->price, 2, ',', ' ') }}
+                                                                        €</small>
+                                                                   @if ( $item->description)
+                                                                       
+                                                                   <p class="txt-xs"
+                                                                       style="line-height: 1.2;">
+                                                                       {{ $item->description ?? $item->description }}
+                                                                   </p>
+                                                                   @endif
+                                                                  
+                                                                </label>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                    <div class="col-6">
+                                                        {{-- <div class="col-6">
                                                         <div class="d-flex justify-content-end">
                                                             <span>{{ number_format($item->price, 2, ',', ' ') }}
                                                                 €</span>
                                                         </div>
+                                                    </div> --}}
                                                     </div>
-                                                </div>
-                                            @endforeach
+                                                @endforeach
+                                            </div>
                                         @endforeach
                                         @error('variant')
                                             <small class="text-danger fw-bold">{{ $message }}</small>
@@ -104,30 +121,39 @@
                                 {{-- <hr class="my-3"> --}}
 
 
-                                <div class="mb-3">
+                                <div class="mb-3 mt-3">
                                     @if (count($selected_product->getChoices()) > 0)
                                         <h6 class="fw-bold mb-3">{{ __('Add-ons') }}</h6>
-                                        @foreach ($selected_product->getChoices() as $item)
-                                            <div class="row">
-                                                <div class="col-6">
-                                                    <div class="form-check mb-3">
-                                                        <input class="form-check-input" type="checkbox"
+
+                                       <div class="d-flex flex-wrap">
+                                         @foreach ($selected_product->getChoices() as $item)
+                                            <div class="">
+                                                <div class="">
+                                                    <div class="fc-wrapper">
+                                                        <input class="fc-input" type="checkbox"
                                                             value="{{ $item->id }}" id="chk{{ $item->id }}"
                                                             wire:model.live="choices" wire:click="calculateTotal">
-                                                        <label class="form-check-label" for="chk{{ $item->id }}">
-                                                            {{ $item->getChoiceName()->Choice_name }}
+                                                        <label class="fc-label" for="chk{{ $item->id }}">
+                                                           <div class="fc-title">
+                                                             {{ $item->getChoiceName()->Choice_name }}
+                                                           </div>
+                                                           <small>
+                                                            {{ number_format($item->price, 2, ',', ' ') }}
+                                                            €
+                                                           </small>
                                                         </label>
                                                     </div>
                                                 </div>
-                                                <div class="col-6">
+                                                {{-- <div class="col-6">
                                                     <div class="d-flex flex-row-reverse">
                                                         <span>{{ number_format($item->price, 2, ',', ' ') }}
                                                             €</span>
 
                                                     </div>
-                                                </div>
+                                                </div> --}}
                                             </div>
                                         @endforeach
+                                       </div>
                                     @endif
 
 
