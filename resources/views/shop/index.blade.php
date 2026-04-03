c880986063ddc69785db83035c73b1b4714c20ea
Lakmal Wijerathna <samdynmic@gmail.com>
2026-04-03 11:21:15 +0200

    new caresol update

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Shop')
 @section('content')
-    
-        {{-- @php
+
+    {{-- @php
             $status = App\Models\ShopStatus::whereDate('closing_date', today())
                 ->where('status_name', 'closed')
                 ->first();
@@ -15,7 +15,7 @@
         </div>
         @endif --}}
 
-    <div class="banner">
+    {{-- <div class="banner">
         <div class="row">
             <div class="col-8">
                 <div class="">
@@ -37,119 +37,217 @@
                 </div>
             </div>
         </div>
+    </div> --}}
+
+
+    <div>
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="banner">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="">
+                                    <div class="hero-banner">
+                                        <div>
+                                            <h1>Uživaj u ukusnoj hrani i osvoji kupone!</h1>
+                                            <p>Dobij kupon za svako jelo iz <strong>M Brothers Food</strong> trucka — požuri
+                                                i registriraj se još danas!</p>
+                                            @guest<a href="{{ route('register') }}" class="btn btn-warning">Registriraj
+                                                se</a>@endguest
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                            <div class="col-4">
+                                <div class="">
+                                    <img src="{{ asset('images/design/philly_cheese.png') }}" alt="burger"
+                                        class="banner-image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="banner" style="background-image: url('{{ asset('images/design/banner2.JPG') }}')">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="">
+                                    <div class="hero-banner" style="background-color: rgba(0, 0, 0, 0.5); height:400px">
+                                        <div>
+                                            <h1>Gladni? 😋</h1>
+                                            <p>Naš food truck u Velikoj Gorici poslužuje sreću, zalogaj po zalogaj. Dođite i pridružite se street food zabavi!</p>
+                                            {{-- @guest<a href="{{ route('register') }}" class="btn btn-warning">Registriraj
+                                                se</a>@endguest --}}
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="carousel-item">
+                    <div class="banner">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="">
+                                    <div class="hero-banner">
+                                        <div>
+                                            <h1>Uživaj u ukusnoj hrani i osvoji kupone!</h1>
+                                            <p>Dobij kupon za svako jelo iz <strong>M Brothers Food</strong> trucka — požuri
+                                                i registriraj se još danas!</p>
+                                            @guest<a href="{{ route('register') }}" class="btn btn-warning">Registriraj
+                                                se</a>@endguest
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                            <div class="col-4">
+                                <div class="">
+                                    <img src="{{ asset('images/design/philly_cheese.png') }}" alt="burger"
+                                        class="banner-image">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> --}}
+            </div>
+            {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button> --}}
+        </div>
     </div>
-    
+
     <div class="d-flex justify-content-center mb-3 mt-3">
-            <div class="d-sm-block d-none">
-                        <div style="">
+        <div class="d-sm-block d-none">
+            <div style="">
 
-            @if (count($categories) > 0)
-                <div class="d-flex mb-3">
-                    {{-- <a href="{{ route('shop.index')}}" class="menu-tab {{ request()->route('category') == null ? 'menu-tab-active' : '' }}">
+                @if (count($categories) > 0)
+                    <div class="d-flex mb-3">
+                        {{-- <a href="{{ route('shop.index')}}" class="menu-tab {{ request()->route('category') == null ? 'menu-tab-active' : '' }}">
                        
                         Sve
                     </a> --}}
-                    <a href="{{ route('shop.index') }}" class="cat-link">
-                        <div class="cat-button">
-                            <div class="">
-                                <div class="d-flex justify-content-center">
-                                    <div class="icon  {{ request()->route('category') == null ? 'cat-active' : '' }}">
-                                        <img src="{{ asset('images/category/all.svg') }}" alt="" class="img-fluid">
-                                    </div>
-                                </div>
-                                <div class="text">
-                                    sve
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    @foreach ($categories as $item)
-                        {{-- <a class="menu-tab {{ request()->route('category') == $item->category_name ? 'menu-tab-active' : '' }}" href="{{route('shop.category',$item->category_name)}}">
-                        {{$item->category_name}}
-                    </a> --}}
-                        <a href="{{ route('shop.category', $item->category_name) }}" class="cat-link">
+                        <a href="{{ route('shop.index') }}" class="cat-link">
                             <div class="cat-button">
                                 <div class="">
                                     <div class="d-flex justify-content-center">
-                                        <div
-                                            class="icon {{ request()->route('category') == $item->category_name ? 'cat-active' : '' }}">
-                                            <img src="{{ asset('images/category/' . $item->image) }}" alt=""
+                                        <div class="icon  {{ request()->route('category') == null ? 'cat-active' : '' }}">
+                                            <img src="{{ asset('images/category/all.svg') }}" alt=""
                                                 class="img-fluid">
                                         </div>
                                     </div>
                                     <div class="text">
-                                        {{ str_replace('-', ' ', $item->category_name) }}
-
+                                        sve
                                     </div>
                                 </div>
                             </div>
                         </a>
-                    @endforeach
+                        @foreach ($categories as $item)
+                            {{-- <a class="menu-tab {{ request()->route('category') == $item->category_name ? 'menu-tab-active' : '' }}" href="{{route('shop.category',$item->category_name)}}">
+                        {{$item->category_name}}
+                    </a> --}}
+                            <a href="{{ route('shop.category', $item->category_name) }}" class="cat-link">
+                                <div class="cat-button">
+                                    <div class="">
+                                        <div class="d-flex justify-content-center">
+                                            <div
+                                                class="icon {{ request()->route('category') == $item->category_name ? 'cat-active' : '' }}">
+                                                <img src="{{ asset('images/category/' . $item->image) }}" alt=""
+                                                    class="img-fluid">
+                                            </div>
+                                        </div>
+                                        <div class="text">
+                                            {{ str_replace('-', ' ', $item->category_name) }}
 
-                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        @endforeach
+
+                    </div>
 
 
-                {{--  --}}
+                    {{--  --}}
 
-                {{--  --}}
-            @endif
-        </div>
+                    {{--  --}}
+                @endif
             </div>
-            <div class="d-sm-none d-block">
-                        <div style="max-width:380px; overflow-x:auto;">
+        </div>
+        <div class="d-sm-none d-block">
+            <div style="max-width:380px; overflow-x:auto;">
 
-            @if (count($categories) > 0)
-                <div class="d-flex mb-3">
-                    {{-- <a href="{{ route('shop.index')}}" class="menu-tab {{ request()->route('category') == null ? 'menu-tab-active' : '' }}">
+                @if (count($categories) > 0)
+                    <div class="d-flex mb-3">
+                        {{-- <a href="{{ route('shop.index')}}" class="menu-tab {{ request()->route('category') == null ? 'menu-tab-active' : '' }}">
                        
                         Sve
                     </a> --}}
-                    <a href="{{ route('shop.index') }}" class="cat-link">
-                        <div class="cat-button">
-                            <div class="">
-                                <div class="d-flex justify-content-center">
-                                    <div class="icon  {{ request()->route('category') == null ? 'cat-active' : '' }}">
-                                        <img src="{{ asset('images/category/all.svg') }}" alt="" class="img-fluid">
-                                    </div>
-                                </div>
-                                <div class="text">
-                                    sve
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                    @foreach ($categories as $item)
-                        {{-- <a class="menu-tab {{ request()->route('category') == $item->category_name ? 'menu-tab-active' : '' }}" href="{{route('shop.category',$item->category_name)}}">
-                        {{$item->category_name}}
-                    </a> --}}
-                        <a href="{{ route('shop.category', $item->category_name) }}" class="cat-link">
+                        <a href="{{ route('shop.index') }}" class="cat-link">
                             <div class="cat-button">
                                 <div class="">
                                     <div class="d-flex justify-content-center">
-                                        <div
-                                            class="icon {{ request()->route('category') == $item->category_name ? 'cat-active' : '' }}">
-                                            <img src="{{ asset('images/category/' . $item->image) }}" alt=""
+                                        <div class="icon  {{ request()->route('category') == null ? 'cat-active' : '' }}">
+                                            <img src="{{ asset('images/category/all.svg') }}" alt=""
                                                 class="img-fluid">
                                         </div>
                                     </div>
                                     <div class="text">
-                                        {{ str_replace('-', ' ', $item->category_name) }}
-
+                                        sve
                                     </div>
                                 </div>
                             </div>
                         </a>
-                    @endforeach
+                        @foreach ($categories as $item)
+                            {{-- <a class="menu-tab {{ request()->route('category') == $item->category_name ? 'menu-tab-active' : '' }}" href="{{route('shop.category',$item->category_name)}}">
+                        {{$item->category_name}}
+                    </a> --}}
+                            <a href="{{ route('shop.category', $item->category_name) }}" class="cat-link">
+                                <div class="cat-button">
+                                    <div class="">
+                                        <div class="d-flex justify-content-center">
+                                            <div
+                                                class="icon {{ request()->route('category') == $item->category_name ? 'cat-active' : '' }}">
+                                                <img src="{{ asset('images/category/' . $item->image) }}" alt=""
+                                                    class="img-fluid">
+                                            </div>
+                                        </div>
+                                        <div class="text">
+                                            {{ str_replace('-', ' ', $item->category_name) }}
 
-                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        @endforeach
+
+                    </div>
 
 
-                {{--  --}}
+                    {{--  --}}
 
-                {{--  --}}
-            @endif
-        </div>
+                    {{--  --}}
+                @endif
             </div>
+        </div>
     </div>
     @livewire('shop.index', ['products' => $products])
     </div>
