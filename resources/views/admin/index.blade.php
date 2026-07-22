6e1adff05555ae2f35e60ee00e8c1fe0541d1acb
Lakmal Wijerathna <samdynmic@gmail.com>
2026-07-22 16:04:44 +0200

    new update admin index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,11 +4,34 @@
     <div class="">
         <div class="container mt-3">
             <div>
-                <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
-                <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.1/dist/Chart.min.js"></script>
+                <div class="row">
+                    <div class="col-lg-8">
+                       <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ __('Total Sales')}}</h5>
+                                <p class="card-text">{{ __('Total Sales for the current month')}}</p>
+                                  <canvas class="my-4" id="salses_chart" width="900" height="380"></canvas>
+                       </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ __('Order Count')}}</h5>
+                            <p class="card-text">{{ __('Number of orders for the current month')}}</p>
+                            <canvas class="my-4" id="salses_count_chart" width="900" height="380"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.1/dist/Chart.min.js"></script>
                 <script>
-                    var ctx = document.getElementById("myChart");
-                    var myChart = new Chart(ctx, {
+                    var ctx = document.getElementById("salses_chart");
+                    var ctx2 = document.getElementById("salses_count_chart");
+
+                    var SalesChart = new Chart(ctx, {
                         type: 'line',
                         data: {
                             labels: @json($data['month']),
@@ -43,8 +66,31 @@
                             }
                         }
                     });
+
+                    var SalesCountChart = new Chart(ctx2, {
+                        type: 'line',
+                        data: {
+                            labels: @json($data['month']),
+                            datasets: [{
+                                label: 'Order Count',
+                                data: @json($data['order_count']),
+                                backgroundColor: '#007bff',
+                                borderColor: '#007bff',
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            },
+                            legend: {
+                                display: false,
+                            }
+                        }
+                    });
                 </script>
-            </div>
-        </div>
-    </div>
 @endsection
