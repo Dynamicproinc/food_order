28f63ccac53bf5083911bf39cd40302d197fdf91
Lakmal Wijerathna <samdynmic@gmail.com>
2026-07-22 16:09:12 +0200

    dsdsdS

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -38,18 +38,18 @@
                             datasets: [{
                                 label: 'Total Sales',
                                 data: @json($data['total_amount']),
-                                lineTension: 0,
+                               
                                 backgroundColor: 'transparent',
                                 borderColor: '#007bff',
-                                borderWidth: 4,
+                                borderWidth: 1,
                                 pointBackgroundColor: '#007bff'
                             }, {
                                 label: 'Order Count',
                                 data: @json($data['order_count']),
-                                lineTension: 0,
+                               
                                 backgroundColor: 'transparent',
                                 borderColor: '#28a745',
-                                borderWidth: 4,
+                                borderWidth: 1,
                                 pointBackgroundColor: '#28a745'
                             }]
                         },
